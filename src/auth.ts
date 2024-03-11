1e9ab32228575602fe713031dbf196fdc9f4526d
Marek Matusica <marek.matusica@goodrequest.com>
2024-03-11 15:11:50 +0100

    Add proper middleware functionality

--- src/auth.ts
+++ src/auth.ts
@@ -8,16 +8,11 @@ export const authConfig = {
         signIn: "/login",
     },
     callbacks: {
-        authorized({ auth, request: { nextUrl } }) {
+        authorized({ auth }) {
             const isLoggedIn = !!auth?.user;
-            const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
-            if (isOnDashboard) {
-                if (isLoggedIn) return true;
-                return false; // Redirect unauthenticated users to login page
-            } else if (isLoggedIn) {
-                return Response.redirect(new URL("/dashboard", nextUrl));
-            }
-            return true;
+            // if user is not logged in, it will automatically redirect to the page specified in the pages.signIn property
+            // (in our case /login)
+            return isLoggedIn;
         },
     },
     providers: [
