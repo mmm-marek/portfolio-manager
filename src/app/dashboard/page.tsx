5f4daea549dba97e62594fa4133666ac2473fc9f
Marek Matusica <marek.matusica@goodrequest.com>
2024-03-11 09:59:19 +0100

    Add initial dashboard route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
-const Home = () => {
+const Dashboard = () => {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                <h1>Index page</h1>
-                <Link href="/dashboard">Link to Dashboard</Link>
+                <h1>Dashboard</h1>
+                <Link href="/">Link to Index</Link>
             </div>
         </main>
     );
 };
 
-export default Home;
+export default Dashboard;
