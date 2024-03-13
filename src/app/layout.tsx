820a46ae464477aea2018e73bb295fd1dac1f467
Marek Matusica <marek.matusica@goodrequest.com>
2024-03-13 09:29:48 +0100

    Install tanstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { AntdRegistry } from "@ant-design/nextjs-registry";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ const RootLayout = ({
     return (
         <html lang="en">
             <body className={inter.className}>
-                <AntdRegistry>{children}</AntdRegistry>
+                <Providers>
+                    <AntdRegistry>{children}</AntdRegistry>
+                </Providers>
             </body>
         </html>
     );
---
+++ src/utils/queryKeyFactory.ts
@@ -0,0 +1,9 @@
+enum QUERY_KEYS {
+    STOCKS_ALL = "STOCKS_ALL",
+}
+
+export const queryKeys = {
+    stocks: {
+        all: [QUERY_KEYS.STOCKS_ALL],
+    },
+};
